5471cc341c228db684a26c2261a29d9a96d3b74b
GotouBocchi <nicezelj@gmail.com>
2025-08-28 19:57:47 +0800

    Update variation3.c

--- arraylist/variation3.c
+++ arraylist/variation3.c
@@ -10,18 +10,6 @@ typedef struct{
 
 }List;
 
-void display(List L) {
-    int i;
-    
-    printf("Data: ");
-    
-    for(i=0; i < L.count; i++) {
-        printf("%d", L.elemPtr[i]);
-    }
-    printf("\nCount: %d\n", L.count);
-    
-}
-
 List initialize(List L) {
     L.max = LENGHT;
     L.elemPtr = malloc(L.max * sizeof(int));
@@ -40,14 +28,61 @@ List insertPos(List L, int data, int position) {
     
     return L;
 }
-List deletePos();
-int locate();
-List instertSorted();
 
-List resize();
+List deletePos(List L, int position) {
+    int i;
+    L.count--;
+    for(i=position; i<L.count; i++) {
+        L.elemPtr[i] = L.elemPtr[i+1];
+    }
+    
+    return L;
+}
+
+int locate(List L, int data) {
+    int i;
+    for(i=0; i<L.count; i++) {
+        if(L.elemPtr[i] == data) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+List insertSorted(List L, int data) {
+    int i, x;
+    L.count ++;
+    for(i = 0; L.elemPtr[i] < data && i < L.count; i++);
+    for(x = L.count-1; x>i && x > i; x--) {
+        L.elemPtr[x] = L.elemPtr[x-1];
+    }
+    L.elemPtr[x] = data;
+    
+    return L;
+}
+
+void display(List L) {
+    int i;
+    
+    printf("Data: ");
+    
+    for(i=0; i < L.count; i++) {
+        printf("%d ", L.elemPtr[i]);
+    }
+    printf("\nCount: %d\n", L.count);
+    
+}
+
+List resize(List L) {
+    int *arr;
+    arr = realloc(L.elemPtr, L.max);
+    L.elemPtr = arr;
+    return L;
+}
 
 int main() {
     List L;
+    int i;
     
     L = initialize(L);
     
@@ -62,6 +97,21 @@ int main() {
     L = insertPos(L, 2, 1);
     
     display(L);
+    
+    L = deletePos(L, 2);
+    
+    display(L);
+    
+    printf("%d\n", locate(L, 1));
+    
+    L = insertSorted(L, 10);
+    
+    display(L);
+    
+    L = resize(L);
+    
+    printf("%d\n", L.max);
+
    
     return 0;
 }
