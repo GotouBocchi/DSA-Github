426c7c53f5d898d710e4e48f61b352eba0979c98
GotouBocchi <nicezelj@gmail.com>
2025-09-08 18:00:00 +0800

    Update Variation1.c

--- Queue Array/Variation1.c
+++ Queue Array/Variation1.c
@@ -35,20 +35,36 @@ bool isEmpty(Queue* q) {
 }
 
 void enqueue(Queue* q, int value) {
-    if(q->list.count == MAX) printf("Q = is full");
+    if(isFull(q)) {
+        printf("Q = is full");
+        return;
+    }
     else if(isEmpty(q)) {
-        q->front = q->rear =+ 1;
+        q->front = q->rear += 1;
     }
     else {
         q->rear = (q->rear+1)%MAX;
     }
     q->list.items[q->rear] = value;
+    q->list.count++;
+}
+
+int dequeue(Queue* q) {
+    if(isEmpty(q)) return -1;
+    else {
+        q->front = (q->front + 1) % MAX;
+        q->list.count--;
+        return q->list.items[(q->front - 1 + MAX) % MAX];
+    }
+    
+    
 }
 
 void display(Queue* q) {
     int i, x;
+    printf("\nQ = ");
     for(i = q->front, x =0; x != q->list.count; i = (i+1)%MAX, x++) {
-        printf("%d", q->list.items[i]);
+        printf("%d ", q->list.items[i]);
     }
 }
 
@@ -57,15 +73,38 @@ int main() {
     Queue* Q = initialize();
     printf("%d\n", isFull(Q));
     printf("%d\n", isEmpty(Q));
+    enqueue(Q, 213);
+    enqueue(Q, 53);
+    enqueue(Q, 67);
+    enqueue(Q, 547);
+    enqueue(Q, 5237);
+    enqueue(Q, 234);
+    enqueue(Q, 643);
+    enqueue(Q, 985);
+    enqueue(Q, 811);
+    enqueue(Q, 9787);
+    display(Q);
+    printf("\n%d", dequeue(Q));
+    printf("\n%d", dequeue(Q));
+    printf("\n%d", dequeue(Q));
+    printf("\n%d", dequeue(Q));
+    display(Q);
+    enqueue(Q, 23);
+    enqueue(Q, 53);
+    display(Q);
+    printf("\n%d", dequeue(Q));
+    printf("\n%d", dequeue(Q));
+    printf("\n%d", dequeue(Q));
+    printf("\n%d", dequeue(Q));
+    printf("\n%d", dequeue(Q));
+    display(Q);
+    printf("\n%d", dequeue(Q));
+    display(Q);
+    printf("\n%d", dequeue(Q));
     
-    Q->list.items[8] = 8;
-    Q->list.items[9] = 9;
-    Q->list.items[0] = 0;
-    Q->list.items[1] = 1;
-    Q->list.items[2] = 2;
-    Q->front = 8;
-    Q->rear = 2;
-    Q->list.count =5;
+    
+    
+
     
     display(Q);
     
