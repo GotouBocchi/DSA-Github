4b2c9a2fd4c8686b43fb41310d961fac8007df01
GotouBocchi <nicezelj@gmail.com>
2025-09-14 12:14:58 +0800

    Update main.c

--- codechum/Array Stack - Warehouse Inventory System/main.c
+++ codechum/Array Stack - Warehouse Inventory System/main.c
@@ -6,13 +6,11 @@
 int main() {
     // write your code here
     Stack S[3];
-    int i, x;
-    for(i = 0; i < 3; i++) initializeStack(&S[i]);
-    int choice = 0;
-    int value = 0;
-    bool check;
+    Stack temp[3];
+    int i, x, choice = 0, value = 0;
+    bool check = false;
     
-
+    for(i = 0; i < 3; i++) initializeStack(&S[i]);
     
     while(choice != 5) {
         printf("--- Warehouse Inventory Management System ---\n");
@@ -23,36 +21,50 @@ int main() {
         printf("5. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
+        
         switch(choice) {
             case 1:
                 printf("Enter container ID to push: ");
                 scanf("%d", &value);
                 for(i = 0; isFull(&S[i]); i++);
-                push(&S[i], value);
-                printf("Container %d pushed successfully.\n\n", value);
+                if(i == 3) {
+                    printf("All Containers are full.\n\n");
+                }
+                else {
+                    push(&S[i], value);
+                    printf("Container %d pushed successfully.\n\n", value);
+                }
                 break;
+                
             case 2:
                 if(isEmpty(&S[0])) {
                     printf("Error: All stacks are empty. Cannot pop container.\n\n");
-                    break;
                 }
                 else {
                     for(i = 0; isFull(&S[i]); i++); 
-                    printf("Container %d popped successfully.\n\n", pop(&S[i]));
-                    break;
+                    if(i == 3) i = 2;
+                    printf("Container %d popped successfully.\n\n",  pop(&S[i]));
                 }
+                break;
 
             case 3: 
                printf("Enter container ID to check: ");
                 scanf("%d", &value);
-                for(i = 0; i < 3; i++) {
-                    for(x = 0; x < MAX; x++) {
-                        if(S[i].items[x] == value) check = true;
+                check = false;
+                for(i = 0; i < 3; i++) temp[i] = S[i];
+                
+                for(i = 0; i < 3 && !isEmpty(&temp[i]) && check == false; i++) {
+                    for(x = temp[i].top; x >= 0 && check == false; x--) {
+                        if(pop(&temp[i]) == value) {
+                            check = true;
+                        }
                     }
                 }
-                if(check == true) printf("Container %d exists in a stack.\n\n", value);
-                else printf("Container %d does not exist in any stack.\n\n", value);
+                
+                if(check) printf("Container %d exists in a stack.\n\n", value);
+                else printf("Container %d does not exist in any stack.\n\n", value); 
                 break;
+                
             case 4:
                 for(i = 0; i < 3; i++) {
                     printf("\n--- Stack %d ---\n", i+1);
@@ -60,9 +72,17 @@ int main() {
                 }
                 printf("\n");
                 break;
+                
+            case 5:
+                printf("Exiting program.");
+                break;
+                
+            default:
+                printf("The number you input is invalid, Try Again.\n\n");
+                break;
         }
     }
-    printf("Exiting program.");
+    
 
 
     return 0;
