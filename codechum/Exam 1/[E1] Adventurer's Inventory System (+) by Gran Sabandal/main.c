970603c1065e4b2306306135debc522339b05c09
GotouBocchi <nicezelj@gmail.com>
2025-09-21 11:12:25 +0800

    Update main.c

--- codechum/Exam 1/[E1] Adventurer's Inventory System (+) by Gran Sabandal/main.c
+++ codechum/Exam 1/[E1] Adventurer's Inventory System (+) by Gran Sabandal/main.c
@@ -5,18 +5,16 @@
 // The Sacred Push Spell
 void push(Stack *s, Item i) {
  // [Task 1] To do code logic here ...
-    if(s->vs->avail != -1) {
-        if(s->currentWeight + i.weight <= s->maxWeight) {
-            int newCell = allocNode(s->vs);
-            if(newCell != -1) {
-                s->currentWeight += i.weight;
-                s->vs->nodes[newCell].item = i;
-                s->vs->nodes[newCell].next = s->top;
-                s->top = newCell;
-                printf("Push '%c' (%.1f): SUCCESS (Total: %.1f/%.1f)\n",  i.itemName, i.weight, s->currentWeight, s->maxWeight);
-            }
-        } else printf("Push '%c' (%.1f): FAILED - Weight limit exceeded\n", i.itemName, i.weight);
-    } else printf("Push '%c' (%.1f): FAILED - No free nodes\n", i.itemName, i.weight);
+    if(s->currentWeight + i.weight <= s->maxWeight) {
+        int newCell = allocNode(s->vs);
+        if(newCell != -1) {
+            s->currentWeight += i.weight;
+            s->vs->nodes[newCell].item = i;
+            s->vs->nodes[newCell].next = s->top;
+            s->top = newCell;
+            printf("Push '%c' (%.1f): SUCCESS (Total: %.1f/%.1f)\n",  i.itemName, i.weight, s->currentWeight, s->maxWeight);
+        } else printf("Push '%c' (%.1f): FAILED - No free nodes\n", i.itemName, i.weight);
+    } else printf("Push '%c' (%.1f): FAILED - Weight limit exceeded\n", i.itemName, i.weight);
 }
 
 // Main adventure function
