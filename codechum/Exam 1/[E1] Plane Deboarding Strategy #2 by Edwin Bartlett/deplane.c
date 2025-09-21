e9ed1604642822f71c8c149158407fe230a8e35e
GotouBocchi <nicezelj@gmail.com>
2025-09-21 21:48:56 +0800

    Update deplane.c

--- codechum/Exam 1/[E1] Plane Deboarding Strategy #2 by Edwin Bartlett/deplane.c
+++ codechum/Exam 1/[E1] Plane Deboarding Strategy #2 by Edwin Bartlett/deplane.c
@@ -6,5 +6,28 @@
 
 // TODO: Write deplanePassengers function here...
 void deplanePassengers(BoardingList* list) {
+    int a, b, c, check = 0;
+    a = 0;
+    b = 3;
+    c = 6;
     
+    printf("\nDeplaning passengers...\n");
+    while(check < list->passengerCount) {
+        if(isEmpty_v1(&list->seatRows[a].ABC)) {
+            pop(&list->seatRows[a].ABC))
+            printf("Deplaning Ben from row %d, seat ABC", a)
+        } else if (isEmpty_v2(&list->seatRows[a].DEF)) {
+            pop_v2(&list->seatRows[a].DEF));
+            printf("Deplaning Ben from row %d, seat DEF", a)
+        }
+        
+        if(isEmpty_v1(&list->seatRows[b].ABC)) {
+            pop(&list->seatRows[b].ABC))
+            printf("Deplaning Ben from row %d, seat ABC", a)
+        } else if (isEmpty_v2(&list->seatRows[a].DEF)) {
+            pop_v2(&list->seatRows[a].DEF));
+            printf("Deplaning Ben from row %d, seat DEF", a)
+        }
+        
+    }
 }
